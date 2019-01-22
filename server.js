a6bb0255550c5ca3189a78acb95cffbae649ae58
Grace <gracekang.93@gmail.com>
2019-01-21 21:41:14 -0800

    get project by ID

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ const errHandler = require('./errHandler');
 server.use(express.json());
 
 // get all projects
-server.get('/api/projects', async (req, res) => {
+server.get('/projects', async (req, res) => {
     try {
         const projects = await db.getProject();
         res.status(200).json(projects);
+    } catch (err) {
+        errHandler(err);
+    }
+});
+
+// get project by ID
+server.get('/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const project = await db.getProject(id);
+        res.status(200).json(project);
     } catch (err) {
         errHandler(err);
     }
